329bc687a3e1c05417144437e741103708f8c969
ヌルポ <Le.Cai-1@student.uts.edu.au>
2018-10-03 00:24:36 +1000

    修改了存课程的结构

--- screens/SearchResultScreen.js
+++ screens/SearchResultScreen.js
@@ -3,6 +3,7 @@ import {Card} from "react-native-elements";
 import {Button, ScrollView, StyleSheet, Text, View} from "react-native";
 import connect from "react-redux/es/connect/connect";
 import {fetchUTSClass} from "../Redux/Actions/PlannerAction";
+import Toast from "react-native-easy-toast";
 
 class SearchResultScreen extends Component {
 
@@ -20,7 +21,8 @@ class SearchResultScreen extends Component {
         super(props);
         this.state = {
             searchTerm:'',
-            data: {}
+            data: {},
+            subject:[]
         };
 
 
@@ -38,71 +40,79 @@ class SearchResultScreen extends Component {
 
     }
     componentWillReceiveProps(nextProps) {
-        this.setState({data:nextProps.subject});
+        let sujectArray = [];
+        Object.keys(nextProps.subject).forEach( (key,idx) =>{
+            sujectArray.push({
+                description:nextProps.subject[key].description,
+                semester:nextProps.subject[key].semester,
+                subject_code:nextProps.subject[key].subject_code,
+            })
+        });
+
+        this.setState({subject:sujectArray});
+
     }
 
 
     render() {
-
-        let results = [];
         let activites = [];
-        /*
-        Object.keys(this.state.data).forEach( (key,idx) =>{
-            //解析课程基本信息
-            results.push(
-            <Card key={idx}
-                title={this.state.data[key].description + ' (' + this.state.data[key].semester + ')'}>
-                <Text>Subject Code: {this.state.data[key].subject_code}</Text>
-                <Text>Semester: {this.state.data[key].semester}</Text>
-            </Card>);
-            //解析Tuturials
 
-        })
-        */
+
+        const queryResults = this.state.subject.map((item,idx)=>(
+            <Card key={idx}
+                  title={item.description + ' (' + item.semester + ')'}>
+                <Text>Subject Code: {item.subject_code}</Text>
+                <Text>Semester: {item.semester}</Text>
+                <Button title="选择" value="选择" onPress={()=>{this.refs.toast.show(`你选择了id ${item.description}`);}}/>
+
+            </Card>
+        ))
+
+
+        /*
         Object.values(this.state.data).map((item,idx)=>(
-            /* 解析 Lecture */
-            results.push(
-                <Card key={idx}
+
+            queryResults.push(
+                [{
+                    "subject_name":item.description,
+                    "subject_code":item.subject_code,
+                    "semester":item.semester,
+                }],
+            ),
+            Object.values(item.activities).map((item,idx)=>(
+                activites.push(
+                    <Card key={idx} title={item.activity_group_code + ' #' + item.activity_code}>
+                        <Text>班号: <Text style={{fontWeight: 'bold'}}>{item.activity_code}</Text></Text>
+                        <Text>类型: <Text style={{fontWeight: 'bold'}}>{item.activity_type}</Text></Text>
+                        <Text>开课: <Text style={{fontWeight: 'bold'}}>{item.start_date}</Text></Text>
+                        <Text>星期: <Text style={{fontWeight: 'bold'}}>{item.day_of_week}</Text></Text>
+                        <Text>时间: <Text style={{fontWeight: 'bold'}}>{item.start_time}</Text></Text>
+                        <Text>时长: <Text style={{fontWeight: 'bold'}}>{item.duration/60}</Text> 小时</Text>
+                        <Text>地点: <Text style={{fontWeight: 'bold'}}>{item.location}</Text></Text>
+                        <Button value='选班' title='选班' onPress={()=>{console.log('ok')}}/>
+                    </Card>
+                )
+
+            ))
+        ))
+        */
+        /*
+            <Card key={idx}
                       title={item.description}>
                     <Text>学期: <Text style={{fontWeight: 'bold'}}>{item.semester}</Text></Text>
                     <Text>课号: <Text style={{fontWeight: 'bold'}}>{item.subject_code}</Text></Text>
                     <Button value='选班' title='选班' onPress={()=>{console.log('ok')}}/>
 
                 </Card>
-            ), /* 解析Tuturial */
-                Object.values(item.activities).map((item,idx)=>(
-                    activites.push(
-                        <Card key={idx} title={item.activity_group_code + ' #' + item.activity_code}>
-                            <Text>班号: <Text style={{fontWeight: 'bold'}}>{item.activity_code}</Text></Text>
-                            <Text>类型: <Text style={{fontWeight: 'bold'}}>{item.activity_type}</Text></Text>
-                            <Text>开课: <Text style={{fontWeight: 'bold'}}>{item.start_date}</Text></Text>
-                            <Text>星期: <Text style={{fontWeight: 'bold'}}>{item.day_of_week}</Text></Text>
-                            <Text>时间: <Text style={{fontWeight: 'bold'}}>{item.start_time}</Text></Text>
-                            <Text>时长: <Text style={{fontWeight: 'bold'}}>{item.duration/60}</Text> 小时</Text>
-                            <Text>地点: <Text style={{fontWeight: 'bold'}}>{item.location}</Text></Text>
-                            <Button value='选班' title='选班' onPress={()=>{console.log('ok')}}/>
-                        </Card>
-                    )
-
-                ))
-        ))
-
+        */
         return (
             <View style={styles.containerView}>
                 <ScrollView style={styles.container}>
-                    <Text style={{textAlign:"center"}}>找到{results.length}个相关的课程</Text>
-                    {results}
-                    {/*activites*/}
+                    <Text style={{textAlign:"center"}}>找到{this.state.subject.length}个相关的课程</Text>
+                    {queryResults}
                 </ScrollView>
-                {/*
-                <ActionButton buttonColor="rgba(231,76,60,1)">
-                    <ActionButton.Item buttonColor='#9b59b6' title="Add Subject" onPress={() => console.log("notes tapped!")}>
-                        <Icon name="md-done-all" style={styles.actionButtonIcon} />
-                    </ActionButton.Item>
 
-                </ActionButton>
-                */}
-
+                <Toast ref="toast" position='top'/>
             </View>
         );
     }
